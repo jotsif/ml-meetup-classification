45b859008af1c4dcb76759a900afd0d8d998f767
Josef Lindman Hörnlund <josef@appdata.biz>
2015-02-27 08:58:55 +0100

    Fixed kdd cup R script

--- kddcup.R
+++ kddcup.R
@@ -3,11 +3,14 @@ library('ROCR')
 library('ggplot2')
 options(scipen = 16)
 ### Get data
+###
+### http://kdd.ics.uci.edu/databases/kddcup99/kddcup99.html
+
 
 kddcupdata <- read.table('kddcup.data', sep = ',', header = FALSE, stringsAsFactors = FALSE)
 
-### Find column names
-kddnames <- scan('./kddcup.names.txt', what = character(), sep = '\n')
+### Find column names - http://kdd.ics.uci.edu/databases/kddcup99/kddcup.names
+kddnames <- scan('./kddcup.names', what = character(), sep = '\n')
 ### Set correct names to dataframe
 names(kddcupdata) <- c(unlist(lapply(kddnames[2:length(kddnames)], function(x) {strsplit(x, split = ':')[[1]][1]})), 'type')
 
@@ -52,14 +55,34 @@ kddcupdata$random <- runif(nrow(kddcupdata))
 ### We do a random forest to see patterns
 
 
-ggplot(data = subset(kddcupdata, random < 0.1 & dst_bytes > 1), aes(x = dst_bytes, fill = service)) + geom_histogram() + scale_x_log10()
-
 ###
 ###  Can't handle text variables so we treat them slightly different
 
 mail.model <- randomForest(x = subset(kddcupdata[, -match(c('mail', 'type', 'service', 'flag', 'protocol_type'), names(kddcupdata))],random < 0.1), y = factor(subset(kddcupdata, random < 0.1)$mail), ntree = 50, verbose = TRUE, importance = TRUE)
 
 varImpPlot(mail.model)
+
+imp <- data.frame(names = dimnames(importance(mail.model))[[1]], importance(mail.model))[order(importance(mail.model)[,3]), ]
+imp$names <- with(imp, factor(names, levels = names, ordered = TRUE))
+
+ggplot(data = imp) + geom_point(aes(x = MeanDecreaseAccuracy, y = names), position = 'identity')
+
+ggsave('importance.pdf')
+
+### Histogram
+
+ggplot(data = subset(kddcupdata, random < 0.1 & dst_bytes > 1), aes(x = dst_bytes, fill = service)) + geom_histogram() + scale_x_log10()
+ggsave('dst_bytes.pdf')
+
+ggplot(data = subset(kddcupdata, random < 0.1), aes(x = srv_diff_host_rate, fill = service)) + geom_histogram() + scale_x_log10()
+ggsave('srv_diff_host_rate.pdf')
+
+### Check logged in
+
+y1 <- with(kddcupdata, tapply(logged_in, service, mean))
+
+ggplot(data = data.frame(y = y1, names = names(y1))) + geom_point(aes(x = names, y = y, col = y > 0.5)) + coord_flip()
+
 ### Try prediction on rest of set.
 
 kddcupdata$prob <- predict(mail.model, subset(kddcupdata[, -match(c('mail', 'type', 'service', 'flag', 'protocol_type'), names(kddcupdata))]), type = 'prob')[, 2]
@@ -70,8 +93,12 @@ with(subset(kddcupdata, random > 0.9), mean((prob - mail)**2)) # MSE really smal
 
 ###
 
+similarity <- with(subset(kddcupdata, random > 0.9), tapply(prob, factor(service), mean))
+similarity <- data.frame(similarity, names = names(similarity))[order(similarity, decreasing = TRUE),]
+similarity$names <- with(similarity, factor(names, levels = names, ordered = TRUE))
 
-
+ggplot(data = similarity[1:20, ], aes(x = similarity, , y = names)) + geom_point() + xlab('mean probability')
+ggsave('prob_service.pdf')
 ### Plots
 
 ggplot(data = subset(kddcupdata, random < 0.1), aes(x = dst_host_same_srv_rate, y = mail)) + geom_smooth()
